bd195ca29895dc1cf756aa387b2ea753cb24c48d
chayaschiffer <chayaschiffer@gmail.com>
2014-09-16 22:21:48 -0400

    Fixed response server and network request format

--- src/NetworkRequest.java
+++ src/NetworkRequest.java
@@ -11,29 +11,17 @@ public class NetworkRequest {
 		Socket socket = new Socket("www.amazon.com", 80);// first parameter is
 															// the ip, or domain
 															// name, and
-
 		// second parameter is port
-
 		InputStream in = socket.getInputStream();
-
 		OutputStream out = socket.getOutputStream();
-
 		// input stream reads data from socket which receives data from servers.
-
 		String request = "GET /index.html\n\n";
-
 		out.write(request.getBytes());
-
 		out.flush();
-
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
 		String line;
-
 		while ((line = reader.readLine()) != null) {
-
 			System.out.println(line);
-
 		}
 
 	}
--- src/ResponseServer.java
+++ src/ResponseServer.java
@@ -9,39 +9,30 @@ import java.net.Socket;
 public class ResponseServer {
 
 	public static void main(String[] args) throws IOException {
-
 		ServerSocket serverSocket = new ServerSocket(8080);
-
+		int counter = 0;
+		while(true){
 		Socket socket = serverSocket.accept();
-
 		InputStream in = socket.getInputStream();
-
 		// the server socket is the part that listens,
-
 		// you listen by calling serverSocket.accept();
-
 		// when someone connects to you, the server is returned.
-
 		// client initiate connections to server
-
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
 		String line;
-
-		while ((line = reader.readLine()) != null) {
-
+		while (!"".equals(line = reader.readLine())) {
 			System.out.println(line);
-
 		}
-
 		OutputStream out = socket.getOutputStream();
-
+		String response = "<h2>This is the " + counter + "th request </h2>";
 		out.write("HELLO WORLD".getBytes());
-
+		out.write("HTTP/101 200 Ok\n".getBytes());
+		out.write(("Content-Length: " + response.length()+"\n\n").getBytes());
+		out.write(response.getBytes());
 		out.flush();
-
 		out.close();
-
+		counter++;
+		}
 	}
 
 }
